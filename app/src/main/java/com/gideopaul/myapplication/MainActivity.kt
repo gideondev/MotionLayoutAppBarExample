e5529b6d1ba31d32fe34101f21cbf5d8811ab6b9
Gideon Paul <a.gideonpaul@gmail.com>
2022-06-15 16:21:03 +0530

    Auto animating after reaching a certain threshold.

--- app/src/main/java/com/gideopaul/myapplication/MainActivity.kt
+++ app/src/main/java/com/gideopaul/myapplication/MainActivity.kt
@@ -3,6 +3,9 @@ package com.gideopaul.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,6 +15,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -45,6 +49,11 @@ private fun DummySection(
 fun MotionLayoutAppBarDemo() {
     val scrollState = rememberScrollState()
 
+    val progress by animateFloatAsState(
+        targetValue = if (scrollState.value > 150) 1f else 0f,
+        tween(500, easing = LinearOutSlowInEasing)
+    )
+
     Scaffold(
         modifier = Modifier
     ) {
@@ -66,11 +75,8 @@ fun MotionLayoutAppBarDemo() {
         MotionLayoutAppBar(
             title = "MotionLayoutActivity",
             subTitle = "Example",
-            progress = getToolbarMotionLayoutProgress(
-                currentScrollPosition = scrollState.value,
-                startOfTransitionZone = 150,
-                endOfTransitionZone = 250
-            ),
+            progress = progress,
+            backgroundColor = Color(0xFF375857)
         )
     }
 }
